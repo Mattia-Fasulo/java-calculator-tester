14ff63fa9be141a10249bcb02197af0adb89f312
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-03-24 14:53:07 +0100

    refactoring CalculatorTest

--- src/test/java/org/fasulino/CalculatorTest.java
+++ src/test/java/org/fasulino/CalculatorTest.java
@@ -8,21 +8,23 @@ class CalculatorTest {
 
     @Test
     void add() {
-        assertEquals( 10.8, 10 + 0.8);
+        assertEquals( 10.800000190734863, Calculator.add(10, 0.8F));
     }
 
     @Test
     void subtract() {
-        assertEquals( 9.2, 10 - 0.8);
+        assertEquals( 9.199999809265137, Calculator.subtract(10, 0.8F));
     }
 
     @Test
     void divide() {
         assertEquals( 2, 2.8 / 1.4);
+        assertTrue(Float.isInfinite(Calculator.divide(10,0)));
     }
 
     @Test
     void multiply() {
-        assertEquals( 9.2, 4.6 * 2);
+        assertEquals( 9.199999809265137, Calculator.multiply(4.6F,2));
+        assertEquals( 0, Calculator.multiply(4.6F,0));
     }
 }
